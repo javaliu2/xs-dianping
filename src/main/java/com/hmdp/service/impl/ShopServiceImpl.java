7062cd675ed69125971b77f33924ce58ec8a8e0c
xs <2359035384@qq.com>
2025-05-28 11:28:23 +0800

    1、缓存击穿 也叫热点key问题，就是一个被高并发访问并且缓存重建业务较复杂的key突然失效了，无数的请求访问会在瞬间给数据库带来巨大的冲击。 2、解决方案： 1）互斥锁 使用redis的setnx命令，set if Not eXists，返回0表示失败，即该字段已经有值，有线程拿着该锁；返回1表示成功，线程拿到锁，进行后续处理 2）逻辑过期

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.constant.MessageConstant;
@@ -39,6 +40,79 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Object queryById(Long id) {
+        // 1、解决 缓存穿透 问题的方案
+//        return cachePenetrationSolution(id);
+        // 2、解决 缓存击穿 问题的方案
+        return cacheBreakdownSolution(id);
+    }
+
+    /**
+     * 使用互斥锁解决缓存击穿问题
+     *
+     * @param id
+     * @return
+     */
+    private Object cacheBreakdownSolution(Long id) {
+        // 1、查询Redis
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        // 使用JSON字符串保存对象
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2、查询结果
+        // 2.1 之前缓存的空对象
+        if (RedisConstants.CACHE_NULL_KEY.equals(shopJson)) {
+            return null;
+        } else if (StrUtil.isNotBlank(shopJson)) {
+            // 2.2 命中，直接返回result
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        // 2.2 未命中，顺序执行
+        // 3、尝试获取互斥锁
+        String lockKey = RedisConstants.LOCK_SHOP_KEY + id;
+        Shop shop = null;
+        try {
+            boolean getLock = tryLock(lockKey);
+            if (!getLock) {
+                Thread.sleep(400);  // 是业务操作时间的几倍，小于业务时间会造成不必要的内存消耗
+                return cacheBreakdownSolution(id);
+            }
+            // 4、拿到互斥锁，查询数据库
+            shop = getById(id);
+            Thread.sleep(100);  // 模拟数据库操作延迟
+            // 4.1 查询到结果
+            if (shop != null) {
+                // 缓存数据
+                stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+            } else {
+                // 4.2 未查询到商户信息，表明该商户不存在，返回null
+                // 补丁：采取缓存空对象的方式避免缓存穿透，设置较小的TTL，一方面避免内存溢出，另一方面最大地减少数据库和缓存的不一致性
+                stringRedisTemplate.opsForValue().set(key, RedisConstants.CACHE_NULL_KEY, RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 5、释放互斥锁
+            unlock(lockKey);
+        }
+        return shop;
+    }
+
+    private boolean tryLock(String key) {
+        // 设置过期时间，防止死锁
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", RedisConstants.LOCK_SHOP_TTL, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    /**
+     * 缓存空对象来解决缓存穿透问题
+     *
+     * @param id
+     * @return
+     */
+    private Object cachePenetrationSolution(Long id) {
         // 1、查询Redis
         String key = RedisConstants.CACHE_SHOP_KEY + id;
         // 使用JSON字符串保存对象
