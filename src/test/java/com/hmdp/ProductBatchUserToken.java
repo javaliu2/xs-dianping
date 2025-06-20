e3fc7e050ca373643774247423ddb2daaa65e839
xs <2359035384@qq.com>
2025-06-20 11:11:42 +0800

    1、1000个线程的并发请求，使用jmeter. 2、ProductBatchUserToken产生的手机号有重复。数据库只插入50条数据左右。redis有1000条，但是key不一样，value一样。因为key是使用UUID生成的。 3、1000个请求，52个用户，每个用户有若干个请求。 4、结果：数据库插入52个订单，库存减少52，correct，wonderful. 5、聚合报告：最小值1，最大值18. 很小啊，视频中是分别是126和835. 我觉得是因为我这才52个用户，有52个数据库插入操作。 6、生成1000个用户，一个用户一次请求，再次实验。1000个用户，200个订单。结果：最小值1，最大值782. 为什么会这样，是因为业务中数据库操作耗时。最小值为1是因为库存不足直接返回了。

--- src/test/java/com/hmdp/ProductBatchUserToken.java
+++ src/test/java/com/hmdp/ProductBatchUserToken.java
@@ -32,6 +32,10 @@ public class ProductBatchUserToken {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 1000个token，若干个用户，因为手机号有重复
+     * @throws IOException
+     */
     @Test
     void batchProductToken() throws IOException {
         final int user_num = 1000;
@@ -86,6 +90,64 @@ public class ProductBatchUserToken {
         }
         // 4、将tokens写入文件
         Path file = Paths.get("tokens");
+        Files.write(file, tokens);
+    }
+
+    /**
+     * 1000个用户
+     * @throws IOException
+     */
+    @Test
+    void batchProductToken2() throws IOException {
+        final int user_num = 1000;
+        List<String> tokens = new ArrayList<>();
+        for (int i = 0; i < user_num; i++) {
+            // 1、发送验证码
+            // 1.1 构造电话号码
+            String phone = "138" + String.format("%08d", i);
+            // 1.2 通过HttpClient向本机服务发起请求
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            String uri = "http://localhost:80/api/user/code";
+            HttpPost post = new HttpPost(uri);
+            // 设置表单参数
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new BasicNameValuePair("phone", phone));
+            post.setEntity(new UrlEncodedFormEntity(params));
+            try {
+                CloseableHttpResponse response = httpClient.execute(post);
+                System.out.println("状态码：" + response.getCode());
+                System.out.println("响应：" + new String(response.getEntity().getContent().readAllBytes()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // 2、登录
+            // 2.1 从redis拿到验证码，构造LoginFormDTO对象，发起请求
+            String key = RedisConstants.LOGIN_CODE_KEY + phone;
+            String auth_code = stringRedisTemplate.opsForValue().get(key);
+            uri = "http://localhost:80/api/user/login";
+            JSONObject json = new JSONObject();
+            json.put("phone", phone);
+            json.put("code", auth_code);
+            String jsonBody = json.toString();
+            post = new HttpPost(uri);
+            post.setHeader("Content-Type", "application/json");
+            post.setEntity(new StringEntity(jsonBody));
+            try {
+                CloseableHttpResponse response = httpClient.execute(post);
+                System.out.println("状态码：" + response.getCode());
+                String responseJson = new String(response.getEntity().getContent().readAllBytes());
+                JSONObject jsonObject = new JSONObject(responseJson);
+                String token = (String) jsonObject.get("data");
+                // 3、将返回结果中的token保存
+                tokens.add(token);
+                System.out.println("响应：" + responseJson);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        // 4、将tokens写入文件
+        Path file = Paths.get("tokens2");
         Files.write(file, tokens);
     }
 }
