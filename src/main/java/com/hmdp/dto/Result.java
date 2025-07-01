672fc82ff0f4dc46ea2d30fb59ef73ab3c5bb265
xs <2359035384@qq.com>
2025-07-01 10:39:17 +0800

    1、画蛇添足导致出现bug 2、前端代码axios.post("/user/login", this.form).then(({ data }) => {}) 针对后端响应{success: true, data: '5c73ea33-7402-4c0b-838e-e0c5c72714db'}，data就是5c73ea33-7402-4c0b-838e-e0c5c72714db，因为这是解构的写法

--- src/main/java/com/hmdp/dto/Result.java
+++ src/main/java/com/hmdp/dto/Result.java
@@ -18,7 +18,14 @@ public class Result {
     public static Result ok(){
         return new Result(true, null, null, null);
     }
-    public static Result ok(String message){
+
+    /**
+     * 错误复现：前端输入手机号和验证码登录成功之后，再次点击‘我的’，重新跳转回登录页面
+     * 原因：下面的ok(String message)方法是我写的，后端返回 Result.ok(token)时，应该调用ok(Object data)方法
+     * 但是因为是重载，所以他选择了ok(String message)方法，导致前端收到的json长这样：
+     * {"success":true,"errorMsg":"f26241ba-a1af-4a64-beae-e495f513fd4f"}
+     */
+    public static Result okWithMessage(String message){
         return new Result(true, message, null, null);
     }
     public static Result ok(Object data){
